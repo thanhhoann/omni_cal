621d16d2d2f1fecebcbd1b88f2089de71ed0b8df
thanhhoan <pdth456@gmail.com>
2022-01-19 21:48:10 +0700

    remove cors

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,7 +1,6 @@
 import React from "react";
 import axios from "axios";
 import { cheerio } from "cheerio";
-import cors from "cors";
 
 export default function CurrencyConverter({ text }) {
   return <></>;
